601fad5696744cd3ba3a749110dc4e85632f40b1
vijayashreepd <vijayashreepd98@gmail.com>
2020-07-20 15:08:17 +0530

    first commit

--- nodecourse/eventBooking/index.js
+++ nodecourse/eventBooking/index.js
@@ -8,21 +8,38 @@ const dateFormat = require('dateformat');
 const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
+const hbs = require('hbs');
+//const express = require('express');
+const app = express();
+
+app.set('view engine', 'hbs');
+
+hbs.registerHelper('if_eq', function(a, b, opts) {
+    if(a%b==0&&a!=0)
+        return opts.fn(this);
+    else
+        return opts.inverse(this);
+});
+
 
 const flash = require('connect-flash');
 require('./src/db/mongoose');
-const app = express();
+//const app = express();
 app.use(express.urlencoded());
 app.use(express.json()); 
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(express.static('./uploads/'));
 
-//LOADING FILES
+
+//LOADING
 var storage = multer.diskStorage({
-  destination: function(req,file,cb) {
-    cb(null, './uploads')
+  destination:function(req,file,cb) {
+    cb(null, 'uploads/');
+    
   },
+
+
   filename:function(req,file,cb){
-    cb(null,file.fieldname+'_'+Date.now()+"_"+file.originalname);
+    cb(null,file.originalname);
   }
 })
 
@@ -34,48 +51,49 @@ var upload = multer({
 const port = process.env.PORT || 3000;
 const customerModel = require('./src/models/registerModel.js');
 const eventList = require('./src/models/eventModel.js');
-// app.get('/' ,(req, res) => {
-//   res.sendfile( './views/home.html');
-// });
-// app.get('/adminLogin',(req, res) => {
-//   res.sendfile('./views/adminLogin.html');
-// });
-// app.post('/adminLogin',async(req,res,next) => {
-// var name = req.body.uname;
-// var password = req.body.password;
-// //res.json({ access: 'logged' });
+app.get('/' ,(req, res) => {
+  res.sendfile( './views/home.html');
+});
+app.get('/adminLogin',(req, res) => {
+  res.sendfile('./views/adminLogin.html');
+});
+app.post('/adminLogin',async(req,res,next) => {
+var name = req.body.uname;
+var password = req.body.password;
+//res.json({ access: 'logged' });
 
-// //  res.send(name+" "+password);
-// if( name === 'admin' && password === 'admin') {
+//  res.send(name+" "+password);
+if( name === 'admin' && password === 'admin') {
  
-//  const events = await eventList.find( { }, {
-//     eventName: 1,
-//     description: 1,
-//     maxNoOfTicket: 1,
-//     bookingStartTime: 1,
-//     bookingEndTime: 1,
-//     cost: 1,
+ const events = await eventList.find( { }, {
+    eventName: 1,
+    description: 1,
+    maxNoOfTicket: 1,
+    bookingStartTime: 1,
+    bookingEndTime: 1,
+    cost: 1,
+  image:1,
+    _id: 1
+  });
+  if (events) {
+    //res.send("hia"+events[0]);
+  //res.sendfile(events[0]); 
+  //res.sendfile('./views/sample.html');
   
-//     _id: 1
-//   });
-//   if (events) {
-    
-//     res.render('eventView.hbs',{
-//       events: events
-//     }).then(()=>{
-//       res.status(204).send()
-//     });
-//   } 
-//  console.log("success");
+    res.render('eventView.hbs',{
+      events: events
+    });
+  } 
+ //console.log("success");
      
-// } else {
-  
-// //   console.log("soory");
-// // res.status(404).send();
-// }
-// const error =  new error("Please provide valid credential");
+} else {
+  res.send("LOGIN FAILED!!!");
+//   console.log("soory");
+// res.status(404).send();
+}
+//const error =  new error("Please provide valid credential");
 
-// });
+});
 app.get('/adminAddingEvent',(req,res) =>{
  res.sendfile('./views/addingEvent.html') 
 });
@@ -89,17 +107,20 @@ app.post('/addingEvent',upload.single('img'),(req,res) => {
   const bookingStartTime = req.body.stime;
   const bookingEndTime = req.body.etime;
   const cost = req.body.cost;
-  const image = req.file;
-  const img = fs.readFileSync(req.file.path);
-  const encoded_imgage = img.toString('base64');
-  
+  const image = req.file.filename;
+  //res.send(bookingStartTime)
+ // res.send(image);
+//res.sendfile(req.file.path)
+  // const img = fs.readFileSync(image);
+  // const encoded_imgage = img.toString('base64');
+//res.send(encoded_imgage);
 if(!image){
   res.send("Please upload a file");
 
 }else{
+    console.log(image);
 //res.send(image);
- 
-  if (encoded_imgage === "" || ename === "" || edetails === "" ||npeople === "" ||bookingStartTime === "" || bookingEndTime === "" || cost === "") {
+  if (image === "" || ename === "" || edetails === "" ||npeople === "" ||bookingStartTime === "" || bookingEndTime === "" || cost === "") {
     return res.send('Please provide valid information ..All are mandatory');
   }
   const addEventDetails = new eventList({
@@ -109,10 +130,10 @@ if(!image){
     bookingStartTime: new Date(bookingStartTime),
     bookingEndTime: new Date(bookingEndTime),
     cost: cost,
-    image : Buffer.from(encoded_imgage,'base64')
+    image : image
   });
   addEventDetails.save().then(() => {
-    res.send('Event added successfully!!!...');
+    return res.send('Event added successfully!!!...');
   }).catch(() => {
     res.send('failed!!!');
   }); 
@@ -123,93 +144,93 @@ app.get('/userRegistration',(req, res) => {
   res.sendfile('./views/userRegistration.html');
 });
 
-// app.post('/userRegistration', async(req, res) => {
-//   var name = req.body.uname;
-//   // res.send(name);
-//   const password = req.body.password;
-//   const cpassword  = req.body.cpassword;
-//   if (password !==cpassword ) {
-//     res.send('password not matching!!!');
-//   }
+app.post('/userRegistration', async(req, res) => {
+  var name = req.body.uname;
+  // res.send(name);
+  const password = req.body.password;
+  const cpassword  = req.body.cpassword;
+  if (password !==cpassword ) {
+    res.send('password not matching!!!');
+  }
   
-//   else {
-//     const registered = await customerModel.findOne({name:name});
-//     if(registered){
-//        res.send("USER ALREADY EXISTS!!!...");
-//     }
-//     else{
+  else {
+    const registered = await customerModel.findOne({name:name});
+    if(registered){
+       res.send("USER ALREADY EXISTS!!!...");
+    }
+    else{
     
-//       const register = new customerModel({
-//       name: name,
-//       password: password
-//     });
-//     register.save().then(async() => {
-//       const today = new Date();
-//       const events = await eventList.find( { bookingStartTime: { $lte: today } ,
-//         bookingEndTime: { $gte: today } }, {
-//         eventName: 1,
-//         description: 1,
-//         maxNoOfTicket: 1,
-//         bookingStartTime: 1,
-//         bookingEndTime: 1,
-//         cost: 1,
-//         likes: 1,
-//         _id: 1
-//         });
-//         if (events) {
-//           res.render('userEventView.hbs', {
-//             events:events,
-//             username:req.body.uname
+      const register = new customerModel({
+      name: name,
+      password: password
+    });
+    register.save().then(async() => {
+      const today = new Date();
+      const events = await eventList.find( { bookingStartTime: { $lte: today } ,
+        bookingEndTime: { $gte: today } }, {
+        eventName: 1,
+        description: 1,
+        maxNoOfTicket: 1,
+        bookingStartTime: 1,
+        bookingEndTime: 1,
+        cost: 1,
+        likes: 1,
+        _id: 1
+        });
+        if (events) {
+          res.render('userEventView.hbs', {
+            events:events,
+            username:req.body.uname
            
-//           });
-//         }
-//     }).catch(() => {
-//       res.send('fail!!! new user not added...');
-//     });
-//  }
-// }
+          });
+        }
+    }).catch(() => {
+      res.send('fail!!! new user not added...');
+    });
+ }
+}
 
 
  
-// });
+});
 
-// app.get('/userLogin',(req, res) => {
+app.get('/userLogin',(req, res) => {
   
-//   res.sendfile('./views/userLogin.html');
-// });
-// app.post('/userLogin',async(req,res)=> {
-// var name = req.body.uname;
-// var password = req.body.password;
-// const user = await customerModel.findOne( {
-//   name:name,
-//   password: password
-// }); 
-// if(user){
-//   const today = new Date();
-//       const events = await eventList.find( { bookingStartTime: { $lte: today } ,
-//         bookingEndTime: { $gte: today } }, {
-//         eventName: 1,
-//         description: 1,
-//         maxNoOfTicket: 1,
-//         bookingStartTime: 1,
-//         bookingEndTime: 1,
-//         cost: 1,
-//         likes: 1,
-//         _id: 1
-//         });
-//         if (events) {
-//           res.render('userEventView.hbs', {
-//             events:events,
-//             username:req.body.uname
+  res.sendfile('./views/userLogin.html');
+});
+app.post('/userLogin',async(req,res)=> {
+var name = req.body.uname;
+var password = req.body.password;
+const user = await customerModel.findOne( {
+  name:name,
+  password: password
+}); 
+if(user){
+  const today = new Date();
+      const events = await eventList.find( { bookingStartTime: { $lte: today } ,
+        bookingEndTime: { $gte: today } }, {
+        eventName: 1,
+        description: 1,
+        maxNoOfTicket: 1,
+        bookingStartTime: 1,
+        bookingEndTime: 1,
+        cost: 1,
+        likes: 1,
+        _id: 1
+        });
+        if (events) {
+          res.render('userEventView.hbs', {
+            events:events,
+            username:req.body.uname
            
-//           });
-//         }
-//       } else {
-//         res.send("LOGIN FAILED!!!!");
-//       }
+          });
+        }
+      } else {
+        res.send("LOGIN FAILED!!!!");
+      }
  
 
-// })
+})
 
 
 
